35cd4880ade531a9dd35c9bb6a9a52cbd342f204
manhtuanthusinh <tuanlaimanh20041@gmail.com>
2025-11-10 09:36:16 +0700

    add customer public class

---
+++ src/main/java/Functional_interface/Customer.java
@@ -0,0 +1,12 @@
+package Functional_interface;
+
+public class Customer {
+
+        public final String CustName;
+        public final String CustPhoneNum;
+
+        public Customer(String custName, String custPhoneNum) {
+            this.CustName = custName;
+            this.CustPhoneNum = custPhoneNum;
+        }
+}
